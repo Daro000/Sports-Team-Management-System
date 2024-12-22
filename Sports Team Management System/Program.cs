b8405dcdab68b74f8b2062923dfd80032272bdc9
Daro <twojemail@example.com>
2024-12-22 22:06:58 +0100

    Utworzenie klasy Player z metodą do aktualizacji wyniku

---
+++ Sports Team Management System/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Player
+{
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public int Score { get; set; }
+
+    public Player(string name, string position, int score)
+    {
+        Name = name;
+        Position = position;
+        Score = score;
+    }
+
+    
+    public void UpdateScore(int newScore)
+    {
+        Score = newScore;
+    }
+}
